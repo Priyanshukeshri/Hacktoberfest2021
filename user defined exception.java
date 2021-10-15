3b0d142301d287409b3c5067420eb3b5b17a336b
priyanshu kumar keshri <priyanshukumarkeshri6@gmail.com>
2021-10-15 09:27:36 +0530

    Create user defined exception.java

---
+++ user defined exception.java
@@ -0,0 +1,46 @@
+/*
+An user defined exception class : UserVerySmallNumException. This exception is
+thrown before performing a/b, on finding that a/b is very small less than 0.00001. Override
+toString method to display appropriate exception error message
+*/
+
+
+import java.util.Scanner;
+
+class UserVerySmallNumException extends Exception{
+    public double a;
+    UserVerySmallNumException(double p)
+    {
+        a=p;
+    }
+    public String toString()
+    {
+        return "My Exception"+a+"is very less than 0.00001 ";
+    }
+};
+public class Main {
+public static void divide( double a,double b) throws Exception
+{
+    if(b<0.0001){
+        throw new UserVerySmallNumException(a);
+    }
+    else {
+        System.out.println("Divide of two number is:"+a/b);
+    }
+}
+    public static void main(String[] args) throws Exception {
+    double a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first value:");
+        a=sc.nextDouble();
+        System.out.println("Enter the second value:");
+        b= sc.nextDouble();
+        try{
+            divide(a,b);
+        }
+        catch (UserVerySmallNumException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
